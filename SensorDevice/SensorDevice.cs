c74edfa0444b83eab798531759dfa43903266514
Stefan Mehnert <StefanMehnert@gmail.com>
2016-03-06 06:14:46 +0100

    Initial sensor interface implementation

---
+++ SensorDevice/SensorDevice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SensorDevice
+{
+    interface SensorDevice
+    {
+        void powerOff();
+        void enable();
+        void disable();
+
+        bool isEnabled();
+        DateTime isEnabledSince();
+        int getFirmwareVersion();
+        long getSystemUptime();
+        int getLastReading();
+        int getMinReading();
+        int getMaxReading();
+        int getAvgReading();
+    }
+}
